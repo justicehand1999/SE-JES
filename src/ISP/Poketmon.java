b8fe2b4c5267d7288cbf15973245d663de4c9889
justice <justicehand1999@gmail.com>
2023-03-18 09:49:32 +0900

    week3 02 violates isp

---
+++ src/ISP/Poketmon.java
@@ -0,0 +1,32 @@
+package ISP;
+
+public interface Poketmon {
+//    isp 위배
+//    violates isp
+    void attack();
+    void defend();
+    void fry();
+    void swim();
+}
+class Pikachu implements Poketmon{
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void fry() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
